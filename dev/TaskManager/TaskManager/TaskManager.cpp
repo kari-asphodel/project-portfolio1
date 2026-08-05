b99e0db7173a9e69456ffa85b33f4d647a67f892
Kari Asphodel <124915307+kari-asphodel@users.noreply.github.com>
2026-08-05 14:48:14 -0400

    added taskmanager, can add tasks and view tasks

--- dev/TaskManager/TaskManager/TaskManager.cpp
+++ dev/TaskManager/TaskManager/TaskManager.cpp
@@ -1,1 +1,24 @@
 #include "TaskManager.h"
+#include <iostream>
+
+void TaskManager::AddTask(std::string title)
+{
+	tasks.push_back(Task(title));
+	std::cout << "\nTask added to the crypt.\n";
+}
+
+void TaskManager::ViewTasks() const
+{
+	std::cout << "\n==== TASK CRYPT ====\n";
+	if (tasks.empty())
+	{
+		std::cout << "The crypt is empty. No tasks haunt you...yet...\n";
+		return;
+	}
+	for (int i = 0; i < tasks.size(); i++)
+	{
+		std::cout << i + 1 << ". "
+			               << tasks[i].GetTitle()
+			               << "\n";
+	}
+}
--- dev/TaskManager/TaskManager/TaskManager.h
+++ dev/TaskManager/TaskManager/TaskManager.h
@@ -1,4 +1,15 @@
 #pragma once
+#include "Task.h"
+#include <string>
+#include <vector>
 class TaskManager
-{};
+{
+private:
+	std::vector<Task> tasks;
+
+public:
+
+	void AddTask(std::string title);
+	void ViewTasks()const;
+};
 
